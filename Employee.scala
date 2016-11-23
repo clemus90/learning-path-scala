63ceabbd274d2b13343dc1e8130e3add0e35bb88
Christian Lemus <crlg90@gmail.com>
2016-11-23 12:29:37 -0500

    Demostrates multiple constructor alternatives

--- Employee.scala
+++ Employee.scala
@@ -1,5 +1,14 @@
 import scala.beans.BeanProperty
 
 class Employee(@BeanProperty val firstName:String,
-               @BeanProperty var lastName:String)
+               @BeanProperty var lastName:String,
+               val title:String){
+   // Atypical form with side effect
+   def this(firstName:String, lastName:String) =  {this(firstName, lastName, "Programmer")
+                                                   println("Side Effect")}
+   // Atypical form
+   //def this(firstName:String, lastName:String) =  {this(firstName, lastName, "Programmer")}
+   // Typical form
+   // def this(firstName:String, lastName:String) = this(firstName, lastName, "Programmer")
+}
 // Warning: Scala Programmers don't like vars
