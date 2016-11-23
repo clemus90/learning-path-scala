c5368dd612b6ee94d92fb0154d254a7643f23133
Christian Lemus <crlg90@gmail.com>
2016-11-23 14:01:06 -0500

    Adds copy method to the Employee Class

--- Employee.scala
+++ Employee.scala
@@ -1,6 +1,15 @@
 import scala.beans.BeanProperty
 
 class Employee(@BeanProperty val firstName:String,
-               @BeanProperty var lastName:String,
-               val title:String = "Programmer")
+               @BeanProperty val lastName:String,
+               val title:String = "Programmer"){
+  def fullName = s"$firstName $lastName"
+
+  def changeLastName(ln:String) = new Employee(firstName, ln, title)
+
+  def copy(firstName:String = this.firstName, lastName:String = this.lastName,
+           title:String = this.title ) = {
+    new Employee(firstName, lastName, title)
+  }
+}
 // Warning: Scala Programmers don't like vars
--- EmployeeScript.scala
+++ EmployeeScript.scala
@@ -1,7 +1,13 @@
 val ada = new Employee("Ada", "Lovelace")
 println(ada.firstName)
 
-ada.lastName = "Byron, Countess of Lovelace"
-println(ada.lastName)
+val newAda = ada.copy(lastName = "Byron, Countess of Lovelace")
+println(newAda.lastName)
 
 val dennis = new Employee(lastName="Ritchie", firstName="Dennis")
+println(s"The first name is ${dennis.firstName}")
+println(s"The last name is ${dennis.lastName}")
+
+println(ada.fullName)
+println(newAda.fullName)
+println(dennis.fullName)
